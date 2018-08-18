6526f8b1a01fcaed22b2d8f641d1137845886e8a
MAX FELDMAN <maxfeldman99@gmail.com>
2018-08-18 16:49:59 +0300

    CacheUnit logic was updated

--- src/com/max/memory/CacheUnit.java
+++ src/com/max/memory/CacheUnit.java
@@ -28,52 +28,52 @@ public class CacheUnit<T> {
 		for(int i=0;i<ids.length;i++) {
 			dataModel = iAlgoCache.getElement(ids[i]); // if we have an id so we get it by id
 			
-			if(dataModel!=null) {
+			if(dataModel!=null) { // if in cache
 				models[i] = dataModel; //add to my arrayList
 			}else {
-				dataModel = dao.find(ids[i]); // if not exist so we search for it by id
-				iAlgoCache.putElement(dataModel.getDataModelId(), dataModel);//and we use it by paging algorithm
-				
-				
+				//dataModel = dao.find(ids[i]); // if not exist so we search for it by id
+				if(dao.find(ids[i])==null) { // if not in cache
+					iAlgoCache.putElement(dataModel.getDataModelId(), dataModel);//and we use it by paging algorithm
+				}else {
+					dao.save(dataModel);
+					models[i] = dataModel;
+				}	
 				//  if cash is not full return the data model
 				// else make the logic for the full cache
 				// if we received something that not null , so we had a page fault and we need to update the file 
-				
-				dao.save(dataModel);
-				models[i] = dataModel;
-				
-				
 			}
 			
 		}
 		
-	
-		
-		
-		
-		
-//	do If IAlgoCache  ids // only touch the datamodels in case don’t exist
-//
-//	If cache is not full
-//	Retrieves page by the DAO// DM Faults
-//	Else
-//	Do logic of full Cache // DM replacements
-//
-//	end;
-//	return dataModels [] // from Cache
 
 		return models;
 
 	}
 
 	public DataModel<T>[] putDataModels(DataModel<T>[] datamodels) {
-
+		long id;
+		for (int i = 0; i < datamodels.length; i++) {
+		
+			iAlgoCache.putElement(datamodels[i].getDataModelId(),  datamodels[i]); //should be datamodels[i].getContent?
+			dao.save(datamodels[i]);
+		}
+		
+		
 		return datamodels;
 
 	}
 
 	public void removeDataModels(Long[] ids) {
-
+		DataModel<T> dataModel;
+		
+		for(int i=0; i<ids.length;i++) {
+			dataModel = iAlgoCache.getElement(ids[i]);
+			iAlgoCache.removeElement(ids[i]);
+			
+			if(dataModel!=null) {
+			dao.delete(dataModel);
+			}
+		}
 	}
 
 }
