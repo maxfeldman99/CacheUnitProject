16223acc56d3d062995ceba66e40e164b5410be5
MAX FELDMAN <maxfeldman99@gmail.com>
2018-08-28 23:55:06 +0300

    Server run info was added

--- src/com/max/server/CacheUnitServerDriver.java
+++ src/com/max/server/CacheUnitServerDriver.java
@@ -1,13 +1,15 @@
 package com.max.server;
 
+import com.max.util.CLI;
+
 public class CacheUnitServerDriver {
-	
-	public CacheUnitServerDriver() {
-		
-	}
-	
-	static void main(String[] args) {
-		
+
+	public static void main(String[] args) {
+		CLI cli = new CLI(System.in, System.out);
+		Server server = new Server();
+		cli.addPropertyChangeListener(server);
+		new Thread(cli).start();
+
 	}
 
 }
--- src/com/max/server/Server.java
+++ src/com/max/server/Server.java
@@ -2,8 +2,17 @@ package com.max.server;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server implements PropertyChangeListener,Runnable {
+	
+	public static final int TIME_OUT_TIME = 10000;
+	public static final int PORT = 12345;
 
 	public Server() {
 		
@@ -14,6 +23,29 @@ public class Server implements PropertyChangeListener,Runnable {
 
 	@Override
 	public void run() {
+		
+		try {
+			ServerSocket serverSocket = new ServerSocket(PORT);
+			Socket socket = serverSocket.accept();
+			serverSocket.setSoTimeout(TIME_OUT_TIME);
+			ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+			ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+			objectOutputStream.writeObject("something");
+			objectOutputStream.flush();
+			String inputMsg=(String)objectInputStream.readObject();
+			System.out.println("message from the client: "+inputMsg);
+			objectOutputStream.writeObject("closing conneciton");
+			objectOutputStream.flush();
+			socket.close();
+			serverSocket.close();
+			
+			
+			
+			
+		} catch (IOException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 	
